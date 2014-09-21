ee54f25108bad5d83ddf4352623e91dea9bdefc5
Chaos <cawiii@me.com>
2014-09-21 17:09:10 -0400

    fix bug

--- chattr.js
+++ chattr.js
@@ -24,6 +24,8 @@ function injectDependencies(){
     // Add jQuery
     if(!("$" in window)){ // Is jquery incuded?
         loadDependency("//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js", loadFirebase);
+    } else {
+        loadFirebase();
     }
 }
 
